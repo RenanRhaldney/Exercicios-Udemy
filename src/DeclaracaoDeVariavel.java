14f605df430ba6f686505d9ded534149508bd32b
Renan Rhaldney <renan.rhaldney@gmail.com>
2021-07-27 20:12:31 -0300

    Aprendendo java aula 1

---
+++ src/DeclaracaoDeVariavel.java
@@ -0,0 +1,16 @@
+
+public class DeclaracaoDeVariavel {
+
+	public static void main(String[] args) {
+		// Criando uma variavel e exibindo.
+		
+		
+		int idade = 26;
+		
+		System.out.print(idade);
+		//OBS:para exibir oque está guardado dentro da variavel, utilizando o System.out.print não precisamos utlizar aspas "".
+		
+
+	}
+
+}
