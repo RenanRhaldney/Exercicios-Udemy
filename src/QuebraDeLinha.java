2d654fef6a168ac0e1c62dca43d94f0c9603d8cb
Renan Rhaldney <renan.rhaldney@gmail.com>
2021-07-28 20:06:18 -0300

    Diferença entre (Print e Println)

---
+++ src/QuebraDeLinha.java
@@ -0,0 +1,13 @@
+
+public class QuebraDeLinha {
+
+	public static void main(String[] args) {
+		// significado do LN no system.out.print
+		
+		System.out.print("Hello Wolrd!");
+		System.out.println("Bom Dia!");
+		System.out.print("Hello Wolrd!");
+
+	}
+
+}
